16b31ec46048c366f63361b7488ed8eb5efef164
Juan Soto <juan@juansoto.me>
2016-03-15 19:07:56 -0400

    Add fare calculating function

---
+++ app/fare/index.js
@@ -0,0 +1,53 @@
+const rates = {
+  bonusPercent: 11,
+  bonusMin: 5.5,
+  rideCost: 2.75,
+  transactionMax: 80
+}
+
+const getChange = cost => Number((cost - Math.trunc(cost)).toFixed(2) * 100)
+
+const isValid = amount => !(getChange(amount) % 5) && (amount > 0)
+
+const createFareMultiples = max => {
+  const maxToSpend = (max > rates.transactionMax) ? rates.transactionMax : max
+
+  let fares = []
+  let fareMultiple = 0
+
+  while (fareMultiple <= maxToSpend) {
+    fareMultiple += rates.rideCost
+    fares.push(fareMultiple)
+  }
+
+  return fares
+}
+
+const amountToAdd = (fare, balanceLeft) => {
+  let amount = fare - balanceLeft
+  let bonus = rates.bonusPercent * 0.01 + 1
+  let amountWithBonus = (amount / bonus)
+
+  let toAdd = (amountWithBonus < rates.bonusMin) ? amount : amountWithBonus
+
+  return toAdd.toFixed(2)
+}
+
+const amountsToAdd = (balanceLeft, maxToSpend) => {
+  if (balanceLeft === null || maxToSpend === null) return []
+
+  let purchases = []
+  let fares = createFareMultiples(maxToSpend)
+
+  for (let i = fares.length - 1; i >= 0; i--) {
+    let toAdd = amountToAdd(fares[i], balanceLeft)
+
+    if (isValid(toAdd)) {
+      purchases.push({amount: toAdd, rides: fares[i] / rates.rideCost})
+    }
+  }
+
+  return purchases
+}
+
+export default amountsToAdd
---
+++ test/fare.js
@@ -0,0 +1,40 @@
+import test from 'ava'
+import fn from '../app/fare'
+
+test('takes $0 balance', t => {
+  let amounts = fn(0, 40)
+
+  t.is(amounts.length, 4)
+  t.is(amounts[0].rides, 11)
+  t.is(amounts[0].amount, '27.25')
+  t.is(amounts[3].rides, 1)
+  t.is(amounts[3].amount, '2.75')
+})
+
+test('calculates the bonus for amounts > the bonus minimum', t => {
+  let amounts = fn(0.01, 20)
+
+  t.ok(Number(amounts[0].amount) >= 5.5)
+})
+
+test('returns amounts without bonus when an amount would not receive a bonus', t => {
+  let maxPurchaseAmountWithoutBonus = 5.5 * 11 / 10.0
+  let amounts = fn(0, maxPurchaseAmountWithoutBonus)
+
+  amounts.forEach(({amount, rides}) => {
+    t.is(Number(amount), 2.75 * rides)
+  })
+})
+
+test('takes a maximum-to-add value', t => {
+  let maxToSpend = 20
+  let amounts = fn(10, maxToSpend)
+
+  t.ok(Number(amounts[0].amount) <= maxToSpend)
+})
+
+test('caps the max-to-add', t => {
+  let amounts = fn(10, 80 + 15)
+
+  t.ok(Number(amounts[0].amount) <= 80)
+})
